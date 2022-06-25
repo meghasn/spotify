245b45ecd48c330bdacc9009276c441ab506968e
meghasn <meghoos99@gmail.com>
2022-06-25 12:34:10 +0530

    deleted

--- my_music_pattern.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-"""
-convert the downloaded json file to csv
-"""
-with open('StreamingHistory0.json', encoding='utf-8') as inputfile:
-    df = pd.read_json(inputfile)
-
-df.to_csv('my_musicpattern.csv', encoding='utf-8', index=False)
